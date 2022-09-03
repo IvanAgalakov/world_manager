5e73f238805f241872ff27b75f154b76abb07016
IvanAgalakov <ivan.se.agalakov@gmail.com>
2022-09-03 09:43:15 -0600

    rudimentary implementation of egui

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,29 @@ use glium::{glutin, Surface, Display, Frame, Program};
 
 pub mod geometry;
 
+
+fn draw_things(dis:&Display, mut target:Frame, pro:&Program) -> Frame {
+    let point1 = Vertex {position: [0.0, 1.0]};
+    let point2 = Vertex {position: [-1.0, -1.0]};
+    let point3 = Vertex {position: [1.0, -1.0]};
+
+    let shape = vec![point1, point2, point3];
+
+    let vertex_buffer = glium::VertexBuffer::new(dis, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    target.draw(&vertex_buffer, &indices, &pro, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+
+    return target;
+}
+
+
 fn main() {
+    let event_loop = glutin::event_loop::EventLoopBuilder::with_user_event().build();
+    let display = create_display(&event_loop);
 
-    
+    let mut egui_glium = egui_glium::EguiGlium::new(&display, &event_loop);
 
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-
-
-    
-    
 
     let vertex_shader_src = r#"
     #version 140
@@ -41,47 +52,93 @@ fn main() {
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
 
-    event_loop.run(move |ev, _, control_flow| {
+    event_loop.run(move |event, _, control_flow| {
+        let mut redraw = || {
+            let mut quit = false;
 
-        let mut target = display.draw();
+            let repaint_after = egui_glium.run(&display, |egui_ctx| {
+                egui::SidePanel::left("my_side_panel").show(egui_ctx, |ui| {
+                    ui.heading("Hello World!");
+                    if ui.button("Quit").clicked() {
+                        quit = true;
+                    }
+                });
+            });
 
-        // clear color
-        target.clear_color(1.0, 1.0, 1.0, 1.0);
+            *control_flow = if quit {
+                glutin::event_loop::ControlFlow::Exit
+            } else if repaint_after.is_zero() {
+                display.gl_window().window().request_redraw();
+                glutin::event_loop::ControlFlow::Poll
+            } else if let Some(repaint_after_instant) =
+                std::time::Instant::now().checked_add(repaint_after)
+            {
+                glutin::event_loop::ControlFlow::WaitUntil(repaint_after_instant)
+            } else {
+                glutin::event_loop::ControlFlow::Wait
+            };
 
+            {
+                use glium::Surface as _;
+                let mut target = display.draw();
 
-        let target = draw_things(&display, target, &program);
+                let color = egui::Rgba::from_rgb(0.1, 0.3, 0.2);
+                target.clear_color(color[0], color[1], color[2], color[3]);
 
+                // draw things behind egui here
+                let mut target = draw_things(&display, target, &program);
 
-        target.finish().unwrap();
+                egui_glium.paint(&display, &mut target);
 
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
+                // draw things on top of egui here
 
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-        match ev {
-            glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::CloseRequested => {
+                target.finish().unwrap();
+            }
+        };
+
+        match event {
+            // Platform-dependent event handlers to workaround a winit bug
+            // See: https://github.com/rust-windowing/winit/issues/987
+            // See: https://github.com/rust-windowing/winit/issues/1619
+            glutin::event::Event::RedrawEventsCleared if cfg!(windows) => redraw(),
+            glutin::event::Event::RedrawRequested(_) if !cfg!(windows) => redraw(),
+
+            glutin::event::Event::WindowEvent { event, .. } => {
+                use glutin::event::WindowEvent;
+                if matches!(event, WindowEvent::CloseRequested | WindowEvent::Destroyed) {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
-                    return;
-                },
-                _ => return,
-            },
+                }
+
+                let event_response = egui_glium.on_event(&event);
+
+                if event_response {
+                    display.gl_window().window().request_redraw();
+                }
+            }
+            glutin::event::Event::NewEvents(glutin::event::StartCause::ResumeTimeReached {
+                ..
+            }) => {
+                display.gl_window().window().request_redraw();
+            }
             _ => (),
         }
     });
 }
 
-fn draw_things(dis:&Display, mut target:Frame, pro:&Program) -> Frame {
-    let point1 = Vertex {position: [0.0, 1.0]};
-    let point2 = Vertex {position: [-1.0, -1.0]};
-    let point3 = Vertex {position: [1.0, -1.0]};
+fn create_display(event_loop: &glutin::event_loop::EventLoop<()>) -> glium::Display {
+    let window_builder = glutin::window::WindowBuilder::new()
+        .with_resizable(true)
+        .with_inner_size(glutin::dpi::LogicalSize {
+            width: 800.0,
+            height: 600.0,
+        })
+        .with_title("egui_glium example");
 
-    let shape = vec![point1, point2, point3];
+    let context_builder = glutin::ContextBuilder::new()
+        .with_depth_buffer(0)
+        .with_srgb(true)
+        .with_stencil_buffer(0)
+        .with_vsync(true);
 
-    let vertex_buffer = glium::VertexBuffer::new(dis, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-    target.draw(&vertex_buffer, &indices, &pro, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
-
-    return target;
+    glium::Display::new(window_builder, context_builder, event_loop).unwrap()
 }
