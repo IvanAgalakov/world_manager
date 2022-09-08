4736470ee74fc84d3b98abc234f5a004b6385dd7
IvanAgalakov <ivan.se.agalakov@gmail.com>
2022-09-08 13:11:51 -0600

    switched from window to side panel

--- src/gui.rs
+++ src/gui.rs
@@ -4,13 +4,13 @@ use crate::info::InputInfo;
 
 
 pub fn run(egui_ctx: &Context, mut input: InputInfo) -> (bool, InputInfo) {
-    let mut test = egui::Window::new("test window");
+    let mut test = egui::SidePanel::left("actions");
     test = test.resizable(false);
 
     let mut quit = false;
 
-    egui::Window::show(test, egui_ctx, |ui| {
-        ui.heading("I love testing window stuff");
+    egui::SidePanel::show(test, egui_ctx, |ui| {
+        ui.heading("Actions");
         if ui.button("Quit").clicked() {
             quit = true;
         }
